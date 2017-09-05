b403c6d46b99e41009a9c560d668a9b4a635f433
frostermrr <froster.mrr@gmail.com>
2017-09-05 10:33:59 -0400

    Completed calc method

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -31,7 +31,11 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      return null;
+        char[] out = input.toCharArray();
+        for (int i = 0; i < input.length(); i++){
+            out[i] = (char)(out[i] + 1);
+        }
+        return String.valueOf(out);
     }
     
     /**
